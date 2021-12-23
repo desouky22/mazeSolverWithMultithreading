b9b96270341d7aeda367e62a13a76b1bb4e45930
desouky22 <63201035+desouky22@users.noreply.github.com>
2021-12-23 14:13:10 +0200

    Create Action.java

---
+++ Action.java
@@ -0,0 +1,20 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+class Action implements ActionListener{
+    public void actionPerformed( ActionEvent e){
+        Object source = e.getSource();
+        for(int x = 0; x<MainClass.size; x++){
+            for(int y = 0; y<MainClass.size; y++){
+                if(source == AppGui.gridArray[x][y]){
+                    if(AppGui.gridArray[x][y].getText() == "1")
+                        AppGui.gridArray[x][y].setText("0");
+                    else 
+                        AppGui.gridArray[x][y].setText("1");
+                }
+            }
+        }
+    }
+}
