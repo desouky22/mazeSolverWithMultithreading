d8a10f9460d4f7b370520ec0b391dc886017b0f5
desouky22 <63201035+desouky22@users.noreply.github.com>
2021-12-26 18:46:50 +0200

    Update MyThread.java

--- MyThread.java
+++ MyThread.java
@@ -27,21 +27,34 @@ public class MyThread implements Runnable {
 
     public static void dfs(int i,int j,int curDis ) throws InterruptedException{
         int  goi  = i + 1 , goj  = j ,godis = curDis + 1;
+        boolean flag1 = false, flag2 = false;
+        
+        
+//        go down
+        MyThread runnner = new MyThread(goi,goj,godis,1);
+        Thread runfromdown = new Thread(runnner);
 
         if(MainClass.valid(goi,goj,godis)){
-            MyThread runnner = new MyThread(goi,goj,godis,1);
-            Thread runfromdown = new Thread(runnner);
+            flag1 = true;
             runfromdown.start();
-            runfromdown.join();
+            AppGui.answerGrid[goi][goj].setBackground(Color.red);
         }
         
+     
+        
+//        go right
         goi = i ;
         goj = j + 1;
+        MyThread runnner2 = new MyThread(goi,goj,godis,2);
+        Thread runfromright = new Thread(runnner2);
+
         if(MainClass.valid(goi,goj,godis)){
-            MyThread runnner = new MyThread(goi,goj,godis,2);
-            Thread runfromright = new Thread(runnner);
+            flag2 = true;
             runfromright.start();
-            runfromright.join();
+            AppGui.answerGrid[goi][goj].setBackground(Color.blue);
         }
+
+        if(flag1) runfromdown.join();
+        if(flag2) runfromright.join();
     }
 }
